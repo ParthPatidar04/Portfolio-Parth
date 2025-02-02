4c4b5c6f48978357c09e21e740fce272b4a82c51
ParthPatidar04 <patidarparth96@gmail.com>
2025-02-02 11:29:41 +0530

    project end

--- portfolioWebsite/src/components/Footer.jsx
+++ portfolioWebsite/src/components/Footer.jsx
@@ -1,25 +1,35 @@
 import React from 'react'
 import whiteTwitter from '../assets/white-twitter.svg'
+import blackTwitter from '../assets/black-twitter.svg'
 import whiteLinkedIn from '../assets/white-linkedin.svg'
+import blackLinkedIn from '../assets/black-linkedin.svg'
 import whiteInstagram from '../assets/white-instagram.svg'
+import blackInstagram from '../assets/black-instagram.svg'
 import whiteGithub from '../assets/white-github.svg'
+import blackGithub from '../assets/black-github.svg'
+import { useTheme } from '../themeContext'
 
 function Footer() {
+
+    const {theme, toggleTheme} = useTheme()
+    console.log(theme);
+    
+
     return (
         <div className='flex flex-col gap-y-1 justify-end md:mb-6 mb-2'>
             <div className='font-bold'>Follow me on:</div>
             <ul className='flex gap-x-2'>
                 <li>
-                    <a href="https://x.com/ParthPatidar004?t=hOHeZDcVUXPs7E1YbhheqQ&s=09"><img className='w-10' src={whiteTwitter} alt="" /></a>
+                    <a href="https://x.com/ParthPatidar004?t=hOHeZDcVUXPs7E1YbhheqQ&s=09"><img className='w-10' src={theme=="dark" ? whiteTwitter : blackTwitter} alt="" /></a>
                 </li>
                 <li>
-                    <a href="https://www.linkedin.com/in/parthpatidar2004?utm_source=share&utm_campaign=share_via&utm_content=pro"><img className='w-10' src={whiteLinkedIn} alt="" /></a>
+                    <a href="https://www.linkedin.com/in/parthpatidar2004?utm_source=share&utm_campaign=share_via&utm_content=pro"><img className='w-10' src={theme=="dark" ? whiteLinkedIn : blackLinkedIn} alt="" /></a>
                 </li>
                 <li>
-                    <a href="https://github.com/ParthPatidar04"><img className='w-10' src={whiteGithub} alt="" /></a>
+                    <a href="https://github.com/ParthPatidar04"><img className='w-10' src={theme=="dark" ? whiteGithub : blackGithub} alt="" /></a>
                 </li>
                 <li>
-                    <a href="https://www.instagram.com/parth_patidar04/"><img className='w-10' src={whiteInstagram} alt="" /></a>
+                    <a href="https://www.instagram.com/parth_patidar04/"><img className='w-10' src={theme=="dark" ? whiteInstagram : blackInstagram} alt="" /></a>
                 </li>
             </ul>
             <div className='font-bold'>&copy; 2025 Parth. All Rights Reserved</div>
--- portfolioWebsite/src/components/Navigation.jsx
+++ portfolioWebsite/src/components/Navigation.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import sun from '../assets/sun.svg'
 import moon from '../assets/moon.svg'
-// import darkMenu from '../assets/dark-menu.svg'
+import darkMenu from '../assets/dark-menu.svg'
 import whiteMenu from '../assets/white-menu.svg'
-// import blackCross from '../assets/black-cross.svg'
+import blackCross from '../assets/black-cross.svg'
 import whiteCross from '../assets/white-cross.svg'
 import {Link} from 'react-router-dom'
 import { useTheme } from '../themeContext'
@@ -11,7 +11,8 @@ import { useTheme } from '../themeContext'
 
 function Navigation() {
 
-   const {isDarkMode, toggleTheme} = useTheme();
+   const {theme, toggleTheme} = useTheme();
+   
    
 
     const [isOpen, setIsOpen] = useState(false);
@@ -24,10 +25,10 @@ function Navigation() {
         <div>
             <nav className='flex justify-between md:justify-around items-center h-12 -[#212121]'>
                 <div className='md:hidden ml-2'>
-                    <img className='cursor-pointer' onClick={toggleSideBar} src={whiteMenu} alt="menu" />
+                    <img className='cursor-pointer' onClick={toggleSideBar} src={theme=="dark"? whiteMenu : darkMenu} alt="menu" />
                 </div>
-                <div className={`md:hidden z-10 fixed left-0 top-0 w-full h-[50%] bg-[#212121] text-white transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform`}>
-                    <img onClick={toggleSideBar} className='absolute top-4 right-4 p-2' src={whiteCross} alt="close" />
+                <div className={`md:hidden z-10 fixed left-0 top-0 w-full h-[50%] ${theme=="dark" ? 'bg-[#212121]' : 'bg-[#FFF]'} ${theme=="dark" ? 'text-[#FFF]' : 'text-[#212121]'} transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform`}>
+                    <img onClick={toggleSideBar} className='absolute top-4 right-4 p-2' src={theme=="dark" ? whiteCross : blackCross} alt="close" />
                     <div className="flex flex-col items-center mt-12 space-y-6">
                         <Link to='/' className="text-lg cursor-pointer">Home</Link>
                         <Link to='/about' className="text-lg cursor-pointer">About</Link>
@@ -44,7 +45,7 @@ function Navigation() {
                     <Link to='/contact' className='cursor-pointer'>Contact</Link>
                 </ul>
                 <div className='mr-2'>
-                      <img onClick={toggleTheme} src={isDarkMode ? sun : moon} alt="" />
+                      <img onClick={toggleTheme} src={theme=="dark" ? sun : moon} alt="" />
                     </div>
             </nav>
         </div>
