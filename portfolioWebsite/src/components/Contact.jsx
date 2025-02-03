9b0734ac2054462be5cdd59e846a54e97a30b2af
ParthPatidar04 <patidarparth96@gmail.com>
2025-02-03 09:08:45 +0530

    edit contact

--- portfolioWebsite/src/components/Contact.jsx
+++ portfolioWebsite/src/components/Contact.jsx
@@ -11,15 +11,15 @@ function Contact() {
             <div className='mt-4 animate-fadeIn'>
                 <div className='mx-14 my-4'>
                     <div className='mb-1'>Name</div>
-                    <input className={`${theme=="dark" ? 'bg-[#292928] border-white' : 'bg-white border-black'} text-white border  p-2 w-[95%] rounded-lg`} type="text" placeholder='peter parker' />
+                    <input className={`${theme=="dark" ? 'bg-[#292928] border-white' : 'bg-white border-black text-black'}  border  p-2 w-[95%] rounded-lg`} type="text" placeholder='peter parker' />
                 </div>
                 <div className='mx-14 my-4'>
                     <div className='mb-1'>Email</div>
-                    <input className={`${theme=="dark" ? 'bg-[#292928] border-white' : 'bg-white border-black'} text-white border  p-2 w-[95%] rounded-lg`} type="text" placeholder='peterparker@gmail.com' />
+                    <input className={`${theme=="dark" ? 'bg-[#292928] border-white' : 'bg-white border-black text-black'}  border  p-2 w-[95%] rounded-lg`} type="text" placeholder='peterparker@gmail.com' />
                 </div>
                 <div className='mx-14 my-4'>
                     <div className='mb-1'>Message</div>
-                    <textarea className={`${theme=="dark" ? 'bg-[#292928] border-white' : 'bg-white border-black'} text-white border   p-2 w-[95%] h-36 rounded-lg resize-none`} placeholder='enter message' name="" id=""></textarea>
+                    <textarea className={`${theme=="dark" ? 'bg-[#292928] border-white' : 'bg-white border-black text-black'} border   p-2 w-[95%] h-36 rounded-lg resize-none`} placeholder='enter message' name="" id=""></textarea>
                 </div>
                 <div className='mx-14 my-4'>
                     <button className='bg-violet-500 w-[95%] p-2 rounded-lg font-bold text-white'>Send Message</button>
