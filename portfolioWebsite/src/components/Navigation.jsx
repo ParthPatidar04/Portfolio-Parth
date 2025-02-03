920f65eccfd80b2b237f49ed3b9332b417917c05
ParthPatidar04 <patidarparth96@gmail.com>
2025-02-03 09:02:42 +0530

    change nav

--- portfolioWebsite/src/components/Navigation.jsx
+++ portfolioWebsite/src/components/Navigation.jsx
@@ -25,7 +25,7 @@ function Navigation() {
         <div>
             <nav className='flex justify-between md:justify-around items-center h-12 -[#212121] mt-2'>
                 <div className='md:hidden ml-2'>
-                    <img className='cursor-pointer' onClick={toggleSideBar} src={theme=="dark"? whiteMenu : darkMenu} alt="menu" />
+                    <img className='cursor-pointer ml-1' onClick={toggleSideBar} src={theme=="dark"? whiteMenu : darkMenu} alt="menu" />
                 </div>
                 <div className={`md:hidden z-10 fixed left-0 top-0 w-full h-[50%] ${theme=="dark" ? 'bg-[#212121]' : 'bg-[#FFF]'} ${theme=="dark" ? 'text-[#FFF]' : 'text-[#212121]'} transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform`}>
                     <img onClick={toggleSideBar} className='absolute top-4 right-4 p-2' src={theme=="dark" ? whiteCross : blackCross} alt="close" />
@@ -45,7 +45,7 @@ function Navigation() {
                     <Link to='/contact' className='cursor-pointer'>Contact</Link>
                 </ul>
                 <div className='mr-2'>
-                      <img onClick={toggleTheme} src={theme=="dark" ? sun : moon} alt="" />
+                      <img className='mr-4' onClick={toggleTheme} src={theme=="dark" ? sun : moon} alt="" />
                     </div>
             </nav>
         </div>
